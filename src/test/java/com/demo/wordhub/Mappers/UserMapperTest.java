eb13e30af11164629b407bbf9d4a9f3f9a8a3952
OuOu <1113117424@qq.com>
2023-10-18 11:15:30 +0800

    test crud of User

--- src/test/java/com/demo/wordhub/Mappers/UserMapperTest.java
+++ src/test/java/com/demo/wordhub/Mappers/UserMapperTest.java
@@ -1,11 +1,14 @@
 package com.demo.wordhub.Mappers;
 
 import com.demo.wordhub.Entities.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -21,8 +24,35 @@ class UserMapperTest {
     @Autowired
     private UserMapper userMapper;
 
+    User getTestUser(){
+        User user = new User();
+        user.setUsername("test_abc");
+        user.setEmail("test@te.st");
+        user.setPassword("Test123");
+        return user;
+    }
+
     @Test
+    @Order(1)
+    void addUser() {
+        // given
+        User user = getTestUser();
+
+        // when
+        int lines = userMapper.addUser(user);
+
+        // then
+        assertEquals(1, lines);
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    @Order(2)
     void getAll() {
+        // given
+        User user = getTestUser();
+        int lines = userMapper.addUser(user);
+
         // when
         List<User> users = userMapper.getAll();
 
@@ -31,15 +61,14 @@ class UserMapperTest {
     }
 
     @Test
-    void addUser() {
+    @Order(3)
+    void deleteUser(){
         // given
-        User user = new User();
-        user.setUsername("test_abc");
-        user.setEmail("test@te.st");
-        user.setPassword("Test123");
+        User user = getTestUser();
+        userMapper.addUser(user);
 
         // when
-        int lines = userMapper.addUser(user);
+        int lines = userMapper.deleteUser(user);
 
         // then
         assertEquals(1, lines);
@@ -53,5 +82,41 @@ class UserMapperTest {
 
         // then
         assertNull(user);
+    }
+
+    @Test
+    void testCRUD(){
+        // given
+        User user = getTestUser();
+
+        // when add
+        int lines = userMapper.addUser(user);
+
+        // then
+        assertEquals(1, lines);
+        assertNotNull(user.getId());
+
+        // when search
+        User user_created = userMapper.getUserById(user.getId());
+
+        // then
+        assertNotNull(user_created);
+        assertEquals(user.getUsername(), user_created.getUsername());
+        assertNull(user_created.getAvatarPath());
+        assertEquals(user_created.getScore(), 0);
+
+        // when search all
+        List<User> users = userMapper.getAll();
+
+        // then
+        Assertions.assertThat(users).contains(user_created);
+
+        // when delete
+        int lines_deleted = userMapper.deleteUser(user_created);
+
+        // then
+        assertEquals(lines_deleted, 1);
+        users = userMapper.getAll();
+        Assertions.assertThat(users).doesNotContain(user_created);
     }
 }
