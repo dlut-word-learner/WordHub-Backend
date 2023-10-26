d662b66b24b5d61d1724a59c7d9cac0e3c8c154c
OuOu2021 <1113117424@qq.com>
2023-10-26 16:37:11 +0800

    Fix: /sessions -> /session in UserSessionControllerTest

--- src/test/java/cn/dlut/conspirer/wordhub/Controllers/UserSessionControllerTest.java
+++ src/test/java/cn/dlut/conspirer/wordhub/Controllers/UserSessionControllerTest.java
@@ -55,7 +55,7 @@ class UserSessionControllerTest {
         when(userService.checkLogin(anyLong(), anyString())).thenReturn(new User(1L, "test", "123456", "1@a.cn", "1.png", 0L, (short) 0));
         when(userService.checkLogin(anyString(), anyString())).thenReturn(new User(1L, "test", "123456", "1@a.cn", "1.png", 0L, (short) 0));
         String s = objectMapper.writeValueAsString(userLoginVo);
-        mvc.perform(MockMvcRequestBuilders.post("/sessions").contentType(MediaType.APPLICATION_JSON).content(s)).andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON).content(s)).andExpect(status().isOk());
         verify(userService).checkLogin(userLoginVo.getUsername(), userLoginVo.getPassword());
     }
 }
