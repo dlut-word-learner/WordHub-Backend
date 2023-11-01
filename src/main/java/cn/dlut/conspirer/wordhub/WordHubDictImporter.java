5a0c9fe1faa791abf1d2aa99ea741ed77c76342b
OuOu2021 <1113117424@qq.com>
2023-11-01 10:42:38 +0800

    Finish WordHubDictImporter

--- src/main/java/cn/dlut/conspirer/wordhub/WordHubDictImporter.java
+++ src/main/java/cn/dlut/conspirer/wordhub/WordHubDictImporter.java
@@ -8,6 +8,7 @@ import cn.dlut.conspirer.wordhub.Services.WordService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,10 +20,10 @@ import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguratio
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 用于向数据库导入json格式单词表的工具类
@@ -45,19 +46,50 @@ public class WordHubDictImporter implements CommandLineRunner {
         log.info("APPLICATION FINISHED");
     }
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
         log.info("WordHubDictImporter启动");
         Arrays.stream(args).forEach(log::info);
-        if(args.length<2)return ;
-        File dictFile = new File(args[0]);
-        String dictName = args[1];
+        String filePath, dictName;
+        Languages lang;
+        if(args.length<3){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Input the language of the dict(English or Japanese): ");
+            lang = Languages.valueOf(scanner.nextLine());
+            System.out.println("Input the name of the dict: ");
+            dictName = scanner.nextLine();
+            System.out.println("Input the path of the dict: ");
+            filePath = scanner.nextLine();
+        }
+        else {
+            filePath = args[0];
+            dictName = args[1];
+            lang = Languages.valueOf(args[2]);
+        }
+
+        File dictFile = new File(filePath);
         JsonNode words = objectMapper.readTree(dictFile);
-        Languages lang = Languages.English;
+
         Dict dict = new Dict(lang, dictName);
         dictService.addDict(dict);
         words.elements().forEachRemaining(x -> {
             try {
-                Word word = objectMapper.treeToValue(words, Word.class);
+                ObjectNode objectNode = (ObjectNode) x;
+                log.info("ori:" + objectNode.toString());
+                ObjectNode extension = objectMapper.createObjectNode();
+                Iterator<Map.Entry<String, JsonNode>> fieldsIterator = objectNode.fields();
+                while (fieldsIterator.hasNext()) {
+                    Map.Entry<String, JsonNode> field = fieldsIterator.next();
+                    if(!field.getKey().equals("name")){
+                        if(field.getKey().equals("trans"))
+                            extension.put("meanings", field.getValue());
+                        else extension.put(field.getKey(), field.getValue());
+                        fieldsIterator.remove();
+                    }
+                }
+                objectNode.put("extension", extension);
+                log.info("After: " + objectNode.toString());
+                Word word = objectMapper.treeToValue(objectNode, Word.class);
                 wordService.addWordToDict(dict.getId(), word);
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
