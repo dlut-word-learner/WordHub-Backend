431797daac22afaca6b7852935e859146bc7a297
OuOu2021 <1113117424@qq.com>
2023-11-01 20:18:31 +0800

    Add card service

---
+++ src/main/java/cn/dlut/conspirer/wordhub/Services/CardService.java
@@ -0,0 +1,23 @@
+package cn.dlut.conspirer.wordhub.Services;
+
+import cn.dlut.conspirer.wordhub.Entities.Card;
+import cn.dlut.conspirer.wordhub.Entities.CardResponse;
+
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author OuOu
+ * @version 1.0
+ */
+public interface CardService {
+    void addCard(Card card);
+    List<Card> getCardsByUser(Long user_id);
+    Card getCardById(Long cardId);
+    List<CardResponse> getResponseByCardId(Long cardId);
+    void addCardResponse(CardResponse response);
+    void editCardContent(Long cardId, String content);
+
+    void setPublic(Long cardId, boolean isPublic);
+}
---
+++ src/main/java/cn/dlut/conspirer/wordhub/Services/Impls/CardServiceImpl.java
@@ -0,0 +1,62 @@
+package cn.dlut.conspirer.wordhub.Services.Impls;
+
+import cn.dlut.conspirer.wordhub.Entities.Card;
+import cn.dlut.conspirer.wordhub.Entities.CardResponse;
+import cn.dlut.conspirer.wordhub.Mappers.CardMapper;
+import cn.dlut.conspirer.wordhub.Services.CardService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author OuOu
+ * @version 1.0
+ */
+@Slf4j
+@Service
+public class CardServiceImpl implements CardService {
+    CardMapper cardMapper;
+    @Autowired
+    CardServiceImpl(CardMapper cardMapper){
+        this.cardMapper = cardMapper;
+    }
+
+    @Override
+    public void addCard(Card card) {
+        cardMapper.addCard(card);
+    }
+
+    @Override
+    public List<Card> getCardsByUser(Long userId) {
+        return cardMapper.getCardsByUser(userId);
+    }
+
+    @Override
+    public Card getCardById(Long cardId) {
+        return cardMapper.getCardById(cardId);
+    }
+
+    @Override
+    public List<CardResponse> getResponseByCardId(Long cardId) {
+        return cardMapper.getResponseByCardId(cardId);
+    }
+
+    @Override
+    public void addCardResponse(CardResponse response) {
+        cardMapper.addCardResponse(response);
+    }
+
+    @Override
+    public void editCardContent(Long cardId, String content) {
+        cardMapper.editCardContent(cardId,content);
+    }
+
+    @Override
+    public void setPublic(Long cardId, boolean isPublic) {
+        cardMapper.setPublic(cardId,isPublic);
+    }
+}
