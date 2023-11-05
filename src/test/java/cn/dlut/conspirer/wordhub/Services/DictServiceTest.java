5433b1a9278d62d023dac103230243b17de728e3
Damijiang123 <damijiang123@foxmail.com>
2023-11-05 21:53:47 +0800

    update DictServiceTest.addDict

--- src/test/java/cn/dlut/conspirer/wordhub/Services/DictServiceTest.java
+++ src/test/java/cn/dlut/conspirer/wordhub/Services/DictServiceTest.java
@@ -1,6 +1,7 @@
 package cn.dlut.conspirer.wordhub.Services;
 
 import cn.dlut.conspirer.wordhub.Entities.Languages;
+import cn.dlut.conspirer.wordhub.Entities.Dict;
 import cn.dlut.conspirer.wordhub.Mappers.DictMapper;
 import cn.dlut.conspirer.wordhub.Services.Impls.DictServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -46,5 +47,26 @@ public class DictServiceTest {
         verify(dictMapper).getDictsByLanguage(testLanguages);
     }
 
+    @Test
+    void getDictionaryById(){
+        long testId = 0L;
+        dictService.getDictionaryById(testId);
+        verify(dictMapper).getDictById(testId);
+    }
 
+    @Test
+    void getWordsToLearn(){
+        long testDictId = 0,testUserId = 0,testNum = 100;
+        dictService.getWordsToLearn(testDictId,testUserId,testNum);
+        verify(dictMapper).getWordsToLearn(testDictId,testUserId,testNum);
+
+    }
+    @Test
+    void addDict(){
+        Languages testLanguages = Languages.Test;
+        String testName = "testName";
+        Dict testDict = new Dict(testLanguages,testName);
+        dictService.addDict(testDict);
+        verify(dictMapper).addDict(testDict);
+    }
 }
